67d202f6ed33bb00e0e8970e854d2a06caf902e6
Brandon Hazelton <hazeltonbw@gmail.com>
2021-09-01 01:50:26 -0700

    remove scrollbar in mobile, add button to hide results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,16 +2,38 @@ import React from "react";
 import { TrackList } from "../TrackList/TrackList.js";
 import "./SearchResults.css";
 
-export class SearchResults extends React.Component {
+class SearchResults extends React.Component {
+	constructor(props) {
+        super(props);
+        this.changeButton = this.changeButton.bind(this);
+		this.state = {
+			hide: "Hide"
+		}
+	}
+
+	changeButton() {
+		const action = this.state.hide === "Show" ? "Hide" : "Show";
+		this.setState( { hide: action } );
+	}
+
 	render() {
 		return (
 			<div className="SearchResults">
-				<TrackList
+				<input 
+					className="hideResults"
+					type="button"
+					value={this.state.hide + " Results"}
+					onClick={this.changeButton}
+					/>
+				{this.state.hide === "Hide" && <TrackList
 					playlistTracks={this.props.searchResults}
 					onAdd={this.props.onAdd}
 					isRemoval={false}
-				/>
+					hide={this.state.hide}
+				/>}
 			</div>
 		);
 	}
 }
+
+export { SearchResults };
