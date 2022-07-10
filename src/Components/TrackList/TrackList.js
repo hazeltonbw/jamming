fbd500c73a91234427c4f81f6f18e5118630ca46
Brandon Hazelton <hazeltonbw@gmail.com>
2022-07-10 04:14:14 -0700

    dont map over undefined stuff

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import { Track } from "../Track/Track.js";
-import "./TrackList.css";
-import { TrackListHeader } from "../TrackListHeader/TrackListHeader.js";
+import React, { Component } from 'react'
+import { Track } from '../Track/Track.js'
+import './TrackList.css'
+import { TrackListHeader } from '../TrackListHeader/TrackListHeader.js'
 export class TrackList extends Component {
-	render() {
-		return (
-			<div className="TrackList">
-				<TrackListHeader />
-				{this.props.playlistTracks.length ? (
-					this.props.playlistTracks.map((track) => (
-						<Track
-							key={track.id}
-							track={track}
-							onAdd={this.props.onAdd}
-							onRemove={this.props.onRemove}
-							isRemoval={this.props.isRemoval}
-						/>
-					))
-				) : (
-					<h2 className="no-results">No tracks found</h2>
-				)}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='TrackList'>
+        <TrackListHeader />
+        {this.props.playlistTracks ? (
+          this.props.playlistTracks.map((track) => (
+            <Track
+              key={track.id}
+              track={track}
+              onAdd={this.props.onAdd}
+              onRemove={this.props.onRemove}
+              isRemoval={this.props.isRemoval}
+            />
+          ))
+        ) : (
+          <h2 className='no-results'>No tracks found</h2>
+        )}
+      </div>
+    )
+  }
 }
